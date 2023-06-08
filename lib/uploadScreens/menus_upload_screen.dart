c79812cee3d17f9082ab25b7174215b2039dfb6d
yahiyam~ <yahiyamuhammed00@gmail.com>
2023-06-09 00:14:30 +0530

    Validation of Menus Upload Form Upload Menu Image and Get Image url as returnSave menu info to Firestore Database

--- lib/uploadScreens/menus_upload_screen.dart
+++ lib/uploadScreens/menus_upload_screen.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fullfill_seller_app/global/global.dart';
 import 'package:fullfill_seller_app/mainScreens/home_screen.dart';
+import 'package:fullfill_seller_app/widgets/error_dialog.dart';
+import 'package:fullfill_seller_app/widgets/progress_bar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as storage_ref;
 
 class MenusUploadScreen extends StatefulWidget {
   const MenusUploadScreen({super.key});
@@ -14,145 +19,14 @@ class MenusUploadScreen extends StatefulWidget {
 class _MenusUploadScreenState extends State<MenusUploadScreen> {
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
+
   TextEditingController shortInfoController = TextEditingController();
   TextEditingController titleController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return imageXFile == null ? defaultScreen() : menusUploadFormScreen();
-  }
+  bool uploading = false;
+  String uniqueIdName = DateTime.now().millisecondsSinceEpoch.toString();
 
-  Widget menusUploadFormScreen() {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            colors: [
-              Colors.cyan,
-              Colors.amber,
-            ],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 0.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp,
-          )),
-        ),
-        title: const Text(
-          "Uploading New Menu",
-          style: TextStyle(fontSize: 20, fontFamily: "Lobster"),
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            clearMenusUploadForm();
-          },
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              // uploading ? null : () => validateUploadForm(),
-            },
-            child: const Text(
-              "Add",
-              style: TextStyle(
-                color: Colors.cyan,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                fontFamily: "Varela",
-                letterSpacing: 3,
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: ListView(
-        children: [
-          // uploading == true ? linearProgress() : const Text(""),
-          SizedBox(
-            height: 230,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: Center(
-              child: AspectRatio(
-                aspectRatio: 16 / 9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: FileImage(File(imageXFile!.path)),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Divider(
-            color: Colors.amber,
-            thickness: 1,
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.perm_device_information,
-              color: Colors.cyan,
-            ),
-            title: SizedBox(
-              width: 250,
-              child: TextField(
-                style: const TextStyle(color: Colors.black),
-                controller: shortInfoController,
-                decoration: const InputDecoration(
-                  hintText: "menu info",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-          const Divider(
-            color: Colors.amber,
-            thickness: 1,
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.title,
-              color: Colors.cyan,
-            ),
-            title: SizedBox(
-              width: 250,
-              child: TextField(
-                style: const TextStyle(color: Colors.black),
-                controller: titleController,
-                decoration: const InputDecoration(
-                  hintText: "menu title",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-          const Divider(
-            color: Colors.amber,
-            thickness: 1,
-          ),
-        ],
-      ),
-    );
-  }
-
-  clearMenusUploadForm() {
-    setState(() {
-      shortInfoController.clear();
-      titleController.clear();
-      imageXFile = null;
-    });
-  }
-
-  Widget defaultScreen() {
+  defaultScreen() {
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
@@ -170,13 +44,10 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
         ),
         title: const Text(
           "Add New Menu",
-          style: TextStyle(
-            fontSize: 30,
-            fontFamily: "Lobster",
-            color: Colors.white,
-          ),
+          style: TextStyle(fontSize: 30, fontFamily: "Lobster"),
         ),
         centerTitle: true,
+        automaticallyImplyLeading: true,
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back,
@@ -300,5 +171,210 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
     setState(() {
       imageXFile;
     });
+  }
+
+  menusUploadFormScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.cyan,
+              Colors.amber,
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          )),
+        ),
+        title: const Text(
+          "Uploading New Menu",
+          style: TextStyle(fontSize: 20, fontFamily: "Lobster"),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            clearMenusUploadForm();
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: uploading ? null : () => validateUploadForm(),
+            child: const Text(
+              "Add",
+              style: TextStyle(
+                color: Colors.cyan,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                fontFamily: "Varela",
+                letterSpacing: 3,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          uploading == true ? linearProgress() : const Text(""),
+          SizedBox(
+            height: 230,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(File(imageXFile!.path)),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.perm_device_information,
+              color: Colors.cyan,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                controller: shortInfoController,
+                decoration: const InputDecoration(
+                  hintText: "menu info",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.title,
+              color: Colors.cyan,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: "menu title",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  clearMenusUploadForm() {
+    setState(() {
+      shortInfoController.clear();
+      titleController.clear();
+      imageXFile = null;
+    });
+  }
+
+  validateUploadForm() async {
+    if (imageXFile != null) {
+      if (shortInfoController.text.isNotEmpty &&
+          titleController.text.isNotEmpty) {
+        setState(() {
+          uploading = true;
+        });
+
+        //upload image
+        String downloadUrl = await uploadImage(File(imageXFile!.path));
+
+        //save info to firestore
+        saveInfo(downloadUrl);
+      } else {
+        showDialog(
+            context: context,
+            builder: (c) {
+              return const ErrorDialog(
+                message: "Please write title and info for menu.",
+              );
+            });
+      }
+    } else {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return const ErrorDialog(
+              message: "Please pick an image for menu.",
+            );
+          });
+    }
+  }
+
+  saveInfo(String downloadUrl) {
+    final ref = FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(sharedPreferences!.getString("uid"))
+        .collection("menus");
+
+    ref.doc(uniqueIdName).set({
+      "menuID": uniqueIdName,
+      "sellerUID": sharedPreferences!.getString("uid"),
+      "menuInfo": shortInfoController.text.toString(),
+      "menuTitle": titleController.text.toString(),
+      "publishedDate": DateTime.now(),
+      "status": "available",
+      "thumbnailUrl": downloadUrl,
+    });
+
+    clearMenusUploadForm();
+
+    setState(() {
+      uniqueIdName = DateTime.now().millisecondsSinceEpoch.toString();
+      uploading = false;
+    });
+  }
+
+  uploadImage(mImageFile) async {
+    storage_ref.Reference reference =
+        storage_ref.FirebaseStorage.instance.ref().child("menus");
+
+    storage_ref.UploadTask uploadTask =
+        reference.child("$uniqueIdName.jpg").putFile(mImageFile);
+
+    storage_ref.TaskSnapshot taskSnapshot =
+        await uploadTask.whenComplete(() {});
+
+    String downloadURL = await taskSnapshot.ref.getDownloadURL();
+
+    return downloadURL;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return imageXFile == null ? defaultScreen() : menusUploadFormScreen();
   }
 }
